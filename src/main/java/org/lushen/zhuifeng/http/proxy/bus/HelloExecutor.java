077f803a72c8458fe37dde13f64340dbe7126786
zfhlm <914589210@qq.com>
2019-08-08 23:40:52 +0800

    接入示例

--- src/main/java/org/lushen/zhuifeng/http/proxy/bus/HelloExecutor.java
+++ src/main/java/org/lushen/zhuifeng/http/proxy/bus/HelloExecutor.java
@@ -7,14 +7,17 @@ import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
+import org.lushen.zhuifeng.http.proxy.server.HttpProxyErrorResolver;
 import org.lushen.zhuifeng.http.proxy.server.HttpProxyExecutor;
 import org.springframework.stereotype.Component;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 
 /**
  * 自定义http事务执行器
@@ -22,7 +25,7 @@ import io.netty.handler.codec.http.HttpResponse;
  * @author hlm
  */
 @Component
-public class HelloExecutor implements HttpProxyExecutor {
+public class HelloExecutor implements HttpProxyExecutor, HttpProxyErrorResolver {
 
 	@Override
 	public HttpResponse doService(HttpRequest request) throws Exception {	
@@ -33,6 +36,17 @@ public class HelloExecutor implements HttpProxyExecutor {
 		response.headers().set(CONTENT_TYPE, "application/json; charset=utf-8");
 		response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
 		return response;
+	}
+
+	@Override
+	public void resolve(ChannelHandlerContext context, HttpRequest request, Throwable cause) {
+		String message = String.format("{\"answer\":\"%s请求出现异常!\"}", request.method().name());
+		ByteBuf buf = Unpooled.wrappedBuffer(message.getBytes());
+		DefaultFullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.valueOf(500), buf);
+		response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+		response.headers().set(CONTENT_TYPE, "application/json; charset=utf-8");
+		context.write(response);
+		context.flush();
 	}
 
 }
